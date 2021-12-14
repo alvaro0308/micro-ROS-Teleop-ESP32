8fe847ff0e93e2392d0b215e28d7587be0e35c1f
Álvaro Mariscal Ávila <49761770+alvaro0308@users.noreply.github.com>
2021-12-14 23:27:57 +0100

    Testing UART

--- int32_publisher/app.c
+++ int32_publisher/app.c
@@ -1,83 +1,165 @@
+/* UART Echo Example
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <driver/gpio.h>
+#include <rcl/error_handling.h>
+#include <rcl/rcl.h>
+#include <rclc/executor.h>
+#include <rclc/rclc.h>
+#include <std_msgs/msg/int32.h>
 #include <stdio.h>
 #include <unistd.h>
 
-#include <rcl/rcl.h>
-#include <rcl/error_handling.h>
-#include <std_msgs/msg/int32.h>
-#include <driver/gpio.h>
-#include <rclc/rclc.h>
-#include <rclc/executor.h>
-
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
 #ifdef ESP_PLATFORM
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #endif
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+/**
+ * This is an example which echos any data it receives on configured UART back
+ * to the sender, with hardware flow control turned off. It does not use UART
+ * driver event queue.
+ *
+ * - Port: configured UART
+ * - Receive (Rx) buffer: on
+ * - Transmit (Tx) buffer: off
+ * - Flow control: off
+ * - Event queue: off
+ * - Pin assignment: see defines below (See Kconfig)
+ */
+
+int CONFIG_EXAMPLE_UART_PORT_NUM = 0;
+int CONFIG_EXAMPLE_UART_BAUD_RATE = 115200;
+int CONFIG_EXAMPLE_UART_RXD = 3;
+int CONFIG_EXAMPLE_UART_TXD = 1;
+int CONFIG_EXAMPLE_TASK_STACK_SIZE = 2048;
+
+#define ECHO_TEST_TXD (CONFIG_EXAMPLE_UART_TXD)
+#define ECHO_TEST_RXD (CONFIG_EXAMPLE_UART_RXD)
+#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+
+#define ECHO_UART_PORT_NUM (CONFIG_EXAMPLE_UART_PORT_NUM)
+#define ECHO_UART_BAUD_RATE (CONFIG_EXAMPLE_UART_BAUD_RATE)
+#define ECHO_TASK_STACK_SIZE (CONFIG_EXAMPLE_TASK_STACK_SIZE)
+
+static const char *TAG = "UART TEST";
+
+#define BUF_SIZE (1024)
+
+#define RCCHECK(fn)                                                 \
+  {                                                                 \
+    rcl_ret_t temp_rc = fn;                                         \
+    if ((temp_rc != RCL_RET_OK)) {                                  \
+      printf("Failed status on line %d: %d. Aborting.\n", __LINE__, \
+             (int)temp_rc);                                         \
+      vTaskDelete(NULL);                                            \
+    }                                                               \
+  }
+#define RCSOFTCHECK(fn)                                               \
+  {                                                                   \
+    rcl_ret_t temp_rc = fn;                                           \
+    if ((temp_rc != RCL_RET_OK)) {                                    \
+      printf("Failed status on line %d: %d. Continuing.\n", __LINE__, \
+             (int)temp_rc);                                           \
+    }                                                                 \
+  }
 #define LED_BUILTIN 31
 
 rcl_publisher_t publisher;
 std_msgs__msg__Int32 msg;
 
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
-{
-	RCLC_UNUSED(last_call_time);
-	if (timer != NULL) {
-		RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-		msg.data++;
-	}
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+    msg.data++;
+  }
+  uint8_t *data = (uint8_t *)malloc(BUF_SIZE);
+
+  // Read data from the UART
+  int len = uart_read_bytes(ECHO_UART_PORT_NUM, data, (BUF_SIZE - 1),
+                            20 / portTICK_PERIOD_MS);
+  // Write data back to the UART
+  uart_write_bytes(ECHO_UART_PORT_NUM, (const char *)data, len);
+  if (len) {
+    data[len] = '\0';
+    ESP_LOGI(TAG, "Recv str: %s", (char *)data);
+  }
+  printf("Data: %s\n", (const char *)data);
 }
 
-void appMain(void * arg)
-{
-	rcl_allocator_t allocator = rcl_get_default_allocator();
-	rclc_support_t support;
+static void echo_task(void *arg) {}
 
-	// create init_options
-	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+void appMain(void) {
+  rcl_allocator_t allocator = rcl_get_default_allocator();
+  rclc_support_t support;
 
-	// create node
-	rcl_node_t node;
-	RCCHECK(rclc_node_init_default(&node, "freertos_int32_publisher", "", &support));
+  // create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-	// create publisher
-	RCCHECK(rclc_publisher_init_default(
-		&publisher,
-		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-		"freertos_int32_publisher"));
+  // create node
+  rcl_node_t node;
+  RCCHECK(
+      rclc_node_init_default(&node, "freertos_int32_publisher", "", &support));
 
-	// create timer,
-	rcl_timer_t timer;
-	const unsigned int timer_timeout = 1000;
-	RCCHECK(rclc_timer_init_default(
-		&timer,
-		&support,
-		RCL_MS_TO_NS(timer_timeout),
-		timer_callback));
+  // create publisher
+  RCCHECK(rclc_publisher_init_default(
+      &publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+      "freertos_int32_publisher"));
 
-	// create executor
-	rclc_executor_t executor;
-	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-	RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  // create timer,
+  rcl_timer_t timer;
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(timer_timeout),
+                                  timer_callback));
 
-	msg.data = 1000;
-	gpio_pad_select_gpio(LED_BUILTIN);
-    gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
+  // create executor
+  rclc_executor_t executor;
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
-	while(1){
-		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-		gpio_set_level(LED_BUILTIN, 0);        /* Apagado */
-        vTaskDelay(5000 / portTICK_PERIOD_MS);   /* Retraso 500ms */
-        gpio_set_level(LED_BUILTIN, 1);        /* encender */
-        vTaskDelay(5000 / portTICK_PERIOD_MS); 
-		usleep(100000);
-	}
+  msg.data = 1000;
+  /* Configure parameters of an UART driver,
+   * communication pins and install the driver */
+  uart_config_t uart_config = {
+      .baud_rate = ECHO_UART_BAUD_RATE,
+      .data_bits = UART_DATA_8_BITS,
+      .parity = UART_PARITY_DISABLE,
+      .stop_bits = UART_STOP_BITS_1,
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+      .source_clk = UART_SCLK_APB,
+  };
+  int intr_alloc_flags = 0;
 
-	// free resources
-	RCCHECK(rcl_publisher_fini(&publisher, &node))
-	RCCHECK(rcl_node_fini(&node))
+#if CONFIG_UART_ISR_IN_IRAM
+  intr_alloc_flags = ESP_INTR_FLAG_IRAM;
+#endif
 
-  	vTaskDelete(NULL);
+  ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0,
+                                      NULL, intr_alloc_flags));
+  ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
+  ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD,
+                               ECHO_TEST_RTS, ECHO_TEST_CTS));
+
+  //   xTaskCreate(echo_task, "uart_echo_task", ECHO_TASK_STACK_SIZE, NULL, 10,
+  //               NULL);
+  while (1) {
+    rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+  }
+
+  // free resources
+  RCCHECK(rcl_publisher_fini(&publisher, &node))
+  RCCHECK(rcl_node_fini(&node))
+
+  vTaskDelete(NULL);
 }
